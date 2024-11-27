52bb14049241e1df7359a8ecc545523eb5db058d
fau55 <farah.hashmi13sk@gmail.com>
2024-11-27 18:00:01 +0530

    improved product

--- src/models/product.js
+++ src/models/product.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose'
-const Schema = mongoose.Schema;
-const productSchema =  new Schema({
-    productName : {type: String},
-    productPrice : {type: Number},
-    productDescription : {type: String},
-    productImage : {type: String},
-    productQuantity : {type: Number},
-    createdOn : {type: Date}
+import { Schema } from 'mongoose';
+const productSchema = new Schema({
+    productName: { type: String },
+    productPrice: { type: Number },
+    productDescription: { type: String },
+    productImage: { type: String },
+    productQuantity: { type: Number },
+    createdOn: { type: Date }
 })
 
-export const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("products", productSchema);
+
+export { Product };
